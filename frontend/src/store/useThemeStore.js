96581f023b78ee9c10765fae41c271416267655e
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2024-11-27 12:58:30 +0530

    Update useThemeStore.js

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,8 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-    setTheme: (theme) =>{
-        localStorage.setItem("chat-theme", theme)
-        set({theme})
-    }
-}))
+  theme: localStorage.getItem("chat-theme") || "cupcake",
+  setTheme: (theme) => {
+    localStorage.setItem("chat-theme", theme);
+    set({ theme });
+  },
+}));
