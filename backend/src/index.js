514f47ea13752e922a344525d676d2d2c6f33e88
Fahed Khan <127182880+12fahed@users.noreply.github.com>
2024-11-27 12:16:04 +0530

    Build Project

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,11 +6,13 @@ import cookieParser from "cookie-parser"
 import cors from "cors"
 import { connectDB } from "./lib/db.js"
 import { app, server} from "./lib/socket.js"
+import path from "path"
 
 // const app = express()
 
 dotenv.config()
 const PORT = process.env.PORT
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
@@ -22,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
  
 
 server.listen(PORT, () =>{
